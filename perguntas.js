df952d41319dc84fab1c3cfd775c593abb38e3da
Evellyn3b <00001105931900sp@al.educacao.sp.gov.br>
2024-10-14 19:59:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Tecnologia',
+    'O que é Github?',
+    'GitHub é uma plataforma de hospedagem de código-fonte e arquivos com controle de versão.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Itália?',
+    'A capital da Itália é Roma.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um footer?',
+    'Footer se entitula a parte do rodapé do seu site web!'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Amor em Inglês?',
+    'Amor em ingles é Love '
+)
